e53e4fe7e49e9702c61c624a39e8d57ad4e65bc8
Alaa <aladdinabidi@gmail.com>
2023-03-24 09:58:04 +0100

    #Lecture 01 - add java hello and intro

---
+++ fundamentals/01-helloWorld/Intro.java
@@ -0,0 +1,31 @@
+
+ 
+ public class Intro {
+  public static void main(String[] args) {
+
+    // variables
+
+    // Strongly typed language
+
+    int myNumber = 12;
+    String name = "joe";
+
+    boolean isAdmin = true;
+
+    // PRIMITIVES DATA TYPES
+    //primitive type of "int". Primitive types contain the minimum requirements to store the variable as an integer. No other built in features are available. The benefit of this is that it takes up less memory. The down side is it has less built in functions available
+    int x = 5;
+    boolean required = false;
+    double tax = 2.7;
+    float lm = 3.4f;
+    char firstLetter = 'a';
+
+    // OBJECT TYPE (Class Wrappers)
+    //Integer is an object type. Different from primitive type "int" b/c it comes with more built in features and still lets you store int types. Benefit is that it gives you more built in functions for the variable. Downside is it takes up more memory
+    Integer y = 16498;
+    String username = "Mike";
+    Boolean isAvailable = true;
+    Double pie = 3.14;
+
+   }
+}
